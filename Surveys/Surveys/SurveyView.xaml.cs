ade415dbf6299e1bb0d9bc57e5b729632f71f03d
Jakub Stępień <qiubon0@gmail.com>
2017-05-27 11:11:48 +0200

    Added Wpf project and base views

---
+++ Surveys/Surveys/SurveyView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Surveys
+{
+    /// <summary>
+    /// Interaction logic for SurveyView.xaml
+    /// </summary>
+    public partial class SurveyView : UserControl
+    {
+        public SurveyView()
+        {
+            InitializeComponent();
+        }
+
+        private void SendClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
