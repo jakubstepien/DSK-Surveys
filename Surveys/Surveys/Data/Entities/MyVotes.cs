0c69f94bd5c418834effe4a44b474011d99a9457
Jakub Stępień <qiubon0@gmail.com>
2017-05-28 12:43:47 +0200

    Added indexes to tables

--- Surveys/Surveys/Data/Entities/MyVotes.cs
+++ Surveys/Surveys/Data/Entities/MyVotes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace Surveys.Data.Entities
         [Key]
         public Guid IdSurvey { get; set; }
 
+        [Index]
         public Guid IdAnswer { get; set; }
     }
 }
