a9dff6e493e240c3a3d6052f3fc683e5184fbf9f
Jakub Stępień <qiubon0@gmail.com>
2017-06-17 12:08:44 +0200

    Added survey end date timer

--- Surveys/Surveys/Views/SurveyView.xaml.cs
+++ Surveys/Surveys/Views/SurveyView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Surveys.Views
 {
@@ -21,11 +22,34 @@ namespace Surveys.Views
     public partial class SurveyView : UserControl
     {
         public IMainWindow MainWindow { get; set; }
-
+        private DateTime? currentSurveyEnd;
 
         public SurveyView()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (currentSurveyEnd.HasValue)
+            {
+                string text = "Ankieta zkończona";
+                var currTime = DateTime.UtcNow;
+                var span = currentSurveyEnd.Value - currTime;
+                if (currTime > currentSurveyEnd)
+                {
+                    //blokada przycisku
+                }
+                else
+                {
+                    text = span.Days + "d " + span.Hours + "h " + span.Minutes + "m " + span.Seconds + "s";
+                }
+                surveyCountDownTimer.Content = text;
+            }
         }
 
         public void LoadSurvey(Models.SurveyModel survey)
@@ -34,6 +58,7 @@ namespace Surveys.Views
             surveyDescription.Content = survey.Description;
             answers.Items.Clear();
             survey.Answers.ForEach(f => answers.Items.Add(f));
+            currentSurveyEnd = survey.EndDateUTC;
         }
 
         private void SendClick(object sender, RoutedEventArgs e)
